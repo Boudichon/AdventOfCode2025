b1a20a495e32063d01c1aaa9c92e90d2a71fbb66
Dominic Cyr <dcyr@donetechno.com>
2025-12-11 19:48:35 -0500

    Day 5 fix

--- AdventOfCode2025/Days/Day05/Solution.cs
+++ AdventOfCode2025/Days/Day05/Solution.cs
@@ -42,37 +42,26 @@ internal class Solution : BaseSolution
             ranges.Add(new(min, max));
         }
 
-        int previousCount;
+        int previousCount = 0;
 
         do
         {
             previousCount = ranges.Count;
 
-            // Shrink
             foreach (var range in ranges.ToList())
             {
-                Console.WriteLine($"Checking range {range.Min}-{range.Max} for shrink.");
-
-                var overlap = ranges.FirstOrDefault(x => !(x.Min == range.Min && x.Max == range.Max) && x.Min <= range.Min && x.Max + 1 >= range.Min);
-
-                Console.WriteLine(overlap is null
-                    ? $"No overlap found for {range.Min}-{range.Max}."
-                    : $"Found overlap {overlap.Min}-{overlap.Max} for {range.Min}-{range.Max}.");
+                var overlap = ranges.FirstOrDefault(x => x != range && x.Min <= range.Min && x.Max + 1 >= range.Min);
 
                 if (overlap is null) continue;
 
                 overlap.Min = Math.Min(overlap.Min, range.Min);
                 overlap.Max = Math.Max(overlap.Max, range.Max);
                 ranges.Remove(range);
-
-                Console.WriteLine($"Shrunk {range.Min}-{range.Max} into {overlap.Min}-{overlap.Max}.");
             }
 
-        } while (ranges.Count == previousCount);
+        } while (ranges.Count != previousCount);
 
         result = ranges.Distinct().Sum(x => x.Max - x.Min + 1);
-
-        Console.WriteLine($"Reduced to ranges: {string.Join(", ", ranges.Distinct().Select(x => $"{x.Min}-{x.Max}"))}.");
 
         return result;
     }
